0c7821b979ae397bb85fba88bd93c0ab36e1b56e
Angel Gabriel Pareja Rivas <Gabrielpareja.26@hotmail.com>
2026-07-18 13:11:10 -0600

    Add files via upload

---
+++ launch linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Iniciando el codificador WEFAX v2..."
+python3 wefax-encoder-v2.py
---
+++ launch windows.bat
@@ -0,0 +1,5 @@
+@echo off
+title Lanzador WeFax Encoder v2
+echo Iniciando el codificador WEFAX...
+python wefax-encoder-v2.py
+pause
